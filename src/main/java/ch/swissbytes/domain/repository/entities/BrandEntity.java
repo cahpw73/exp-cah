b0515b2ee2728e412c21f0f117ea5209d9ca2e5c
christian_alba <christian.alba@swissbytes.ch>
2015-05-11 15:26:39 -0400

    Added brand entity and service

---
+++ src/main/java/ch/swissbytes/domain/repository/entities/BrandEntity.java
@@ -0,0 +1,83 @@
+package ch.swissbytes.domain.repository.entities;
+
+/**
+ * Created by alvaro on 9/8/14.
+ */
+
+import ch.swissbytes.domain.repository.types.StatusEnum;
+
+import javax.inject.Named;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Named
+@Entity
+@Table(name = "supplier")
+public class BrandEntity implements Serializable{
+
+    private Long id;
+    private String name;
+    private StatusEnum status;
+    private Date lastUpdate;
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
+    @SequenceGenerator(name = "generator", sequenceName = "BRAND_ID_SEQ", allocationSize = 1)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Size(max = 250)
+    @Column(name = "name", nullable = false, length = 250)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column (name = "status",nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    @Column(name = "last_update",nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BrandEntity)) return false;
+
+        BrandEntity that = (BrandEntity) o;
+        return that.hashCode()==this.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (lastUpdate != null ? lastUpdate.hashCode() : 0);
+        return result;
+    }
+}
