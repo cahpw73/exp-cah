f2c5b6945653d060d9265d446bd316c328a17634
christian_alba <christian.alba@swissbytes.ch>
2015-05-15 18:25:56 -0400

    Modified script to dll and added new data initial

--- src/main/java/ch/swissbytes/procurement/boundary/User/UserBean.java
+++ src/main/java/ch/swissbytes/procurement/boundary/User/UserBean.java
@@ -1,10 +1,19 @@
 package ch.swissbytes.procurement.boundary.User;
 
+import ch.swissbytes.Service.business.user.UserService;
+import ch.swissbytes.domain.model.entities.ModuleGrantedAccessEntity;
+import ch.swissbytes.domain.model.entities.UserEntity;
+import ch.swissbytes.domain.model.entities.UserRoleEntity;
+import ch.swissbytes.fqmes.boundary.user.UserEditBean;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -16,12 +25,31 @@ public class UserBean implements Serializable {
 
     public static final Logger log = Logger.getLogger(UserBean.class.getName());
 
+    @Inject
+    private UserService userService;
+
+    private UserEntity newUser;
+
+    private UserEntity editUser;
+
+    private List<ModuleGrantedAccessEntity> moduleGrantedAccessList;
+
+    private List<UserRoleEntity> userRoleList;
+
     private boolean isCreateUser;
+
+    private boolean activeUser;
+
+    private boolean procurementAccess;
+
+    private boolean expeditingAccess;
 
     @PostConstruct
     public void init (){
         log.info("UserBean created");
-
+        newUser = new UserEntity();
+        moduleGrantedAccessList = new ArrayList<>();
+        userRoleList  = new ArrayList<>();
     }
 
     @PreDestroy
