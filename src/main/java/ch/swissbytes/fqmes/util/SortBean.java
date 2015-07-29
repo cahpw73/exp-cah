1a21d37a2bbaac4b6eaed0106ed61db159d8a838
christian_alba <christian.alba@swissbytes.ch>
2015-07-29 13:14:48 -0400

    some changes

--- src/main/java/ch/swissbytes/fqmes/util/SortBean.java
+++ src/main/java/ch/swissbytes/fqmes/util/SortBean.java
@@ -41,19 +41,6 @@ public class SortBean implements Serializable {
         Collections.sort(list, comparator);
     }
 
-    public void sortItemEntity(List<ItemEntity> list){
-        Comparator<ItemEntity> comparator = new Comparator<ItemEntity>() {
-            @Override
-            public int compare(ItemEntity ie1, ItemEntity ie2) {
-                if(StringUtils.isNotEmpty(ie1.getItemNo()) && StringUtils.isNotEmpty(ie2.getItemNo())){
-                    return sortItemNumber(ie1.getItemNo(), ie2.getItemNo());
-                }
-                return 0;
-            }
-        };
-        Collections.sort(list, comparator);
-    }
-
     public void sortCashFlowDetailEntity(List<CashflowDetailEntity> list){
         Comparator<CashflowDetailEntity> comparator = new Comparator<CashflowDetailEntity>() {
             @Override
