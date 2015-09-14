90e4d4e9a5f61e8a3a0c74c00dc129612d8f02e7
Alvaro Cardozo <alvaromcardozo@gmail.com>
2015-09-14 10:19:16 -0400

    Fixing validations

---
+++ src/main/java/ch/swissbytes/fqm/boundary/TableBean.java
@@ -0,0 +1,32 @@
+package ch.swissbytes.fqm.boundary;
+
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.event.data.PageEvent;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by alvaro on 9/14/2015.
+ */
+@Named
+@SessionScoped
+public class TableBean implements Serializable{
+
+    private String defaultPageSize="5";
+
+    public void onPaginate(PageEvent event){
+        System.out.println("Pagination.....");
+        DataTable table=(DataTable)event.getSource();
+        defaultPageSize=Integer.toString(table.getRows());
+    }
+
+    public String getDefaultPageSize() {
+        return defaultPageSize;
+    }
+
+    public void setDefaultPageSize(String defaultPageSize) {
+        this.defaultPageSize = defaultPageSize;
+    }
+}
