4d861e48ef5a7d87effdbd32650adff991ac0fe4
christian_alba <christian.alba@swissbytes.ch>
2015-09-18 14:07:57 -0400

    Added parameter to order by in supplierContactInformation

--- src/main/java/ch/swissbytes/procurement/report/ReportSupplierContactInformation.java
+++ src/main/java/ch/swissbytes/procurement/report/ReportSupplierContactInformation.java
@@ -4,40 +4,21 @@ package ch.swissbytes.procurement.report;
 
 import ch.swissbytes.domain.model.entities.ProjectEntity;
 import ch.swissbytes.fqmes.util.Configuration;
+
+import java.io.Serializable;
 import java.util.Locale;
 import java.util.Map;
 
 /**
  * Created by christian on 11/06/14.
  */
-public class ReportSupplierContactInformation extends ReportProject {
+public class ReportSupplierContactInformation extends ReportProject implements Serializable {
     public ReportSupplierContactInformation(String filenameJasper, String reportNameMsgKey, Map<String, String> messages, Locale locale,
                                             Configuration configuration, ProjectEntity project, final Map<String, Boolean> sortMap) {
         super(filenameJasper, reportNameMsgKey, messages, locale,configuration,project,sortMap);
-
     }
 
     protected void loadAdditionalParameters() {
         addParameters("SUBREPORT_DIR","reports/procurement/supplierContactInformation/");
-        addParameters("PROJECT_ID", project.getId());
-    }
-
-    protected String getStrSort(){
-        Boolean poNo = sortMap.get("poNo");
-        Boolean supplier = sortMap.get("supplier");
-        String strSort = "";
-        if(poNo){
-            strSort = strSort+"po,";
-        }
-        if (supplier){
-            strSort = strSort+"company,";
-        }
-        if(strSort.length()>1){
-            strSort = strSort.substring(0,strSort.length() - 1);
-        }
-        if(strSort.isEmpty()){
-            strSort ="id";
-        }
-        return strSort;
     }
 }
