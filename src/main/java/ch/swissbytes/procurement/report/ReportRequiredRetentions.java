1260d078be5a6c689fe1383acffcce153e961e0a
christian_alba <christian.alba@swissbytes.ch>
2015-09-18 15:14:26 -0400

    Modified the filter in requiredRetentionReport

--- src/main/java/ch/swissbytes/procurement/report/ReportRequiredRetentions.java
+++ src/main/java/ch/swissbytes/procurement/report/ReportRequiredRetentions.java
@@ -17,16 +17,7 @@ import java.util.logging.Logger;
 /**
  * Created by christian on 11/06/14.
  */
-public class ReportRequiredRetentions extends ReportView implements Serializable {
-
-    private final Logger log = Logger.getLogger(ReportRequiredRetentions.class.getName());
-    private ResourceBundle bundle = ResourceBundle.getBundle("messages_en");
-    private Configuration configuration;
-    private ProjectEntity project;
-    Map<String, Boolean> sortMap;
-    private String sortByName="";
-
-
+public class ReportRequiredRetentions extends ReportProject implements Serializable {
 
     /**
      * @param filenameJasper   - fileName the reports to use
@@ -36,61 +27,12 @@ public class ReportRequiredRetentions extends ReportView implements Serializable
      */
     public ReportRequiredRetentions(String filenameJasper, String reportNameMsgKey, Map<String, String> messages, Locale locale,
                                     Configuration configuration, ProjectEntity project, final Map<String, Boolean> sortMap) {
-        super(filenameJasper, reportNameMsgKey, messages, locale);
-        this.configuration = configuration;
-        this.project = project;
-        this.sortMap = sortMap;
-        LookupValueFactory lookupValueFactory = new LookupValueFactory();
-        //addParameters("TIME_MEASUREMENT",lookupValueFactory.geTimesMeasurement());
-        //addParameters("LANGUAGE_LOCALE", configuration.getLanguage());
-        //addParameters("COUNTRY_LOCALE", configuration.getCountry());
-        addParameters("patternDecimal", configuration.getPatternDecimal());
-        addParameters("FORMAT_DATE", configuration.getFormatDate());
-        addParameters("FORMAT_DATE_TIME", configuration.getFormatDateTime());
-        addParameters("TIME_ZONE", configuration.getTimeZone());
+        super(filenameJasper, reportNameMsgKey, messages, locale,configuration,project,sortMap);
+        loadAdditionalParameters();
+    }
+
+    protected void loadAdditionalParameters() {
         addParameters("SUBREPORT_DIR","reports/procurement/RequiredRetentionReport/");
-        loadParamDeliverables();
-    }
-
-    private void loadParamDeliverables() {
-        if(project.getClient()!=null && project.getClient().getClientLogo()!=null){
-            InputStream logo = new ByteArrayInputStream(project.getClient().getClientLogo().getFile());
-            addParameters("logoFooter", logo);
-        }
-        addParameters("projectCode", project.getProjectNumber());
-        addParameters("projectName", project.getTitle());
-        addParameters("projectCurrency",getCurrencyDefault());
-        addParameters("sortBy", getStrSort());
-        addParameters("sortByName",sortByName);
-        addParameters("projectIdFilter",project.getId());
-        Date now = new Date();
-        addParameters("currentDate", Util.convertUTC(now, configuration.getTimeZone()));
-    }
-
-    private String getStrSort(){
-        Boolean poNo = sortMap.get("poNo");
-        Boolean varNo = sortMap.get("varNo");
-        Boolean supplier = sortMap.get("supplier");
-        String strSort = "";
-        if(poNo){
-            strSort = strSort+"po.po, ";
-            sortByName = sortByName + "Po No, ";
-        }
-        if(varNo){
-            strSort = strSort+"po.orderedvariation, ";
-            sortByName = sortByName +  "Var No,";
-        }
-        if (supplier){
-            strSort = strSort+"sp.company, ";
-            sortByName = sortByName +  "Supplier, ";
-        }
-
-        if(strSort.length()>1){
-            sortByName = sortByName.substring(0,sortByName.length()-2);
-            strSort = strSort.substring(0,strSort.length() - 1);
-        }
-        System.out.println("sortBy create: " + strSort);
-        return strSort;
     }
 
     @Override
@@ -100,16 +42,5 @@ public class ReportRequiredRetentions extends ReportView implements Serializable
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
-
-
-    public String getCurrencyDefault() {
-        String currencyDefault = "";
-        for(ProjectCurrencyEntity pc : project.getCurrencies()){
-            if(pc.getProjectDefault()){
-                currencyDefault = pc.getCurrency().getName();
-            }
-        }
-        return currencyDefault;
     }
 }
