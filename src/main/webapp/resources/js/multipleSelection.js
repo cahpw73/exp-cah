4ce173711f71e8d5b3360ed58ed517a7da8961d4
christian_alba <christian.alba@swissbytes.ch>
2016-02-22 17:45:02 -0400

    Implementing multiple status in PO, fixing javascript to show in dropBox

--- src/main/webapp/resources/js/multipleSelection.js
+++ src/main/webapp/resources/js/multipleSelection.js
@@ -6,29 +6,35 @@ $(document).ready(function () {
     initializeMultiselection();
 });
 
-function initializeMultiselection(idsSelected){
-    var array=[];
-    console.log('selected first print '+idsSelected);
+function initializeMultiselection(idsSelected) {
+    var array = [];
+    console.log('selected first print ' + idsSelected);
     selectedValues = [];
+    if (idsSelected) {
+        selectedValues = idsSelected.split(',')
+        $('#poStatusesHidenId').val(selectedValues.toString());
+    }
+    console.log('selected values ' + selectedValues);
     $('#poStatuses').multiselect(
         {
             onChange: function (option, checked, select) {
                 console.log("value affected " + option.val());
                 if (checked) {
+                    console.log("selectedValues Length= " + selectedValues.length);
                     selectedValues[selectedValues.length] = option.val();
                 } else {
-                    index=selectedValues.indexOf(option.val());
-                    selectedValues.splice(index,1);
+                    index = selectedValues.indexOf(option.val());
+                    selectedValues.splice(index, 1);
                 }
                 $('#poStatusesHidenId').val(selectedValues.toString());
             },
-            numberDisplayed:2,
-            buttonClass:'form-control multiselect-button-po'
+            numberDisplayed: 2,
+            buttonClass: 'form-control multiselect-button-po'
         }
     );
-    if(idsSelected){
+    if (idsSelected) {
         array = JSON.parse("[" + idsSelected + "]");
-        console.log('second print selected '+array);
-        $('#poStatuses').multiselect('select',array);
+        console.log('second print selected ' + array);
+        $('#poStatuses').multiselect('select', array);
     }
 }
