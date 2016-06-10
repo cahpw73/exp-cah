ccd2a25bc6519b3000e6f991ee6412893d1370c4
christian_alba <christian.alba@swissbytes.ch>
2016-06-10 09:55:32 -0400

    Fixed title on print Po

--- src/main/java/ch/swissbytes/procurement/report/ReportPurchaseOrder.java
+++ src/main/java/ch/swissbytes/procurement/report/ReportPurchaseOrder.java
@@ -130,12 +130,15 @@ public class ReportPurchaseOrder extends ReportView implements Serializable {
         }
         if (po.getPurchaseOrderProcurementEntity().getClazz() != null) {
             if (po.getPurchaseOrderProcurementEntity().getClazz().ordinal() == ClassEnum.PO.ordinal() || po.getPurchaseOrderProcurementEntity().getClazz().ordinal() == ClassEnum.MINING_FLEET.ordinal()) {
-                addParameters("titleReport", "Purchase Order Summary");
+                addParameters("titleReport", "Purchase Order");
+                addParameters("titlePoSummary", "Purchase Order Summary");
             } else {
-                addParameters("titleReport", "Contract Summary");
+                addParameters("titleReport", "Contract");
+                addParameters("titlePoSummary", "Contract Summary");
             }
         } else {
-            addParameters("titleReport", "Purchase Order Summary");
+            addParameters("titleReport", "Purchase Order");
+            addParameters("titlePoSummary", "Purchase Order Summary");
         }
 
         addParameters("poList", createDataSource(getPOReportDto()));
